c05915341126bfc5968d84b1d379fbba3df29c8d
Gerard Tamakloe <gerrytam@eircom.net>
2012-06-08 21:54:12 +0100

    ASP.NET MVC3 Project
    Initial project set up - see Programme Project Setup document for
    description.

---
+++ Amenity/Amenity/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Amenity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to Rate myAmenities!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Tweet()
+        {
+            return View();
+        }
+    }
+}
